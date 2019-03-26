6dc32540a48769db81ae83fde11af588ebe41b29
mamechapa <mamechapa@mamechapas-iMac.local>
2019-03-26 10:27:17 -0700

    Initial Commit

---
+++ reproductor/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  reproductor
+//
+//  Created by mamechapa on 26/03/2019.
+//  Copyright © 2019 mamechapa. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
